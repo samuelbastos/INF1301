c7070b40fb772c415dbb622d0154bc6591077aed
Sam Bastos <samjr014@gmail.com>
2017-04-30 21:30:23 -0300

    Módulo Tarefa

---
+++ T2/TAREFA.C
@@ -0,0 +1,236 @@
+/***************************************************************************
+*  $MCI Módulo de implementação: Módulo árvore
+*
+*  Arquivo gerado:              TAREFA.C
+*  Letras identificadoras:      TRF
+*
+*  Nome da base de software:    Exemplo de teste automatizado
+*  Arquivo da base de software: D:\AUTOTEST\PROJETOS\SIMPLES.BSW
+*
+*  Projeto: T1 INF1301 2017.1
+*  Autores: mv Maurício Vieira
+*           sb Samuel Bastos
+*
+***************************************************************************/
+
+#include   <malloc.h>
+#include   <stdio.h>
+#include   <string.h>
+
+#define TAREFA_OWN
+#include "TAREFA.H"
+#include "LISTA.H"
+#undef TAREFA_OWN
+
+/***********************************************************************
+*
+*  $TC Tipo de dados: ARV Descritor do nó da árvore
+*
+*
+*  $ED Descrição do tipo
+*     Descreve a organização do nó
+*
+***********************************************************************/
+
+   typedef struct tgTarefa {
+
+       int id;
+
+       char * nome;
+
+       char * descricao;
+
+       void * recurso;
+                
+       LIS_tppLista tarefasPredecessoras ;
+        
+       LIS_tppLista tarefasSucessoras;
+
+   } tpTarefa ;
+
+   static int  idAtual = 0;
+   static TRF_tpCondRet DesconectaTarefas( tpTarefa ** ptTarefa ) ;
+
+/***************************************************************************
+*
+*  Função: TRF Criar tarefa
+*  ****/
+
+   TRF_tpCondRet TRF_CriarTarefa( tpTarefa ** ptTarefa, char * novoNome, char * novaDescricao )
+   {
+
+      tpTarefa * pTarefa = (*ptTarefa);
+      if ( pTarefa != NULL )
+      {
+         TRF_DestruirTarefa( ptTarefa ) ;
+      } /* if */
+
+      pTarefa = ( tpTarefa * ) malloc( sizeof( tpTarefa )) ;
+      if ( pTarefa == NULL )
+      {
+         return TRF_CondRetFaltouMemoria ;
+      } /* if */
+      
+      pTarefa->tarefasPredecessoras = LIS_CriarLista(NULL);
+      pTarefa->tarefasSucessoras = LIS_CriarLista(NULL);
+      LIS_EsvaziarLista( pTarefa->tarefasPredecessoras );
+      LIS_EsvaziarLista( pTarefa->tarefasSucessoras );
+      pTarefa->id = idAtual;
+      pTarefa->nome = (char*)malloc(strlen(novoNome));
+      pTarefa->descricao = (char*)malloc(strlen(novaDescricao));
+      strcpy(pTarefa->nome, novoNome);
+      strcpy(pTarefa->descricao, novaDescricao);
+
+      idAtual += 1;
+
+      (*ptTarefa) = pTarefa;
+
+      return TRF_CondRetOK ;
+
+   } /* Fim função: TRF Criar tarefa */
+
+/***************************************************************************
+*
+*  Função: TRF Destruir tarefa
+*  ****/
+
+   void TRF_DestruirTarefa( tpTarefa ** ptTarefa )
+   {
+     tpTarefa * pTarefa = (*ptTarefa);
+     if ( ptTarefa != NULL)
+     {
+        if ( pTarefa != NULL )
+        {
+          DesconectaTarefas( ptTarefa );
+
+          LIS_EsvaziarLista( pTarefa->tarefasPredecessoras );
+          LIS_DestruirLista( pTarefa->tarefasPredecessoras );
+          LIS_EsvaziarLista( pTarefa->tarefasSucessoras );
+          LIS_DestruirLista( pTarefa->tarefasSucessoras );
+
+          free( pTarefa );
+          (*ptTarefa) = NULL;
+        } /* if */
+     }
+
+   } /* Fim função: TRF Destruir tarefa */
+
+/***************************************************************************
+*
+*  Função: TRF Conectar tarefas
+*  ****/
+
+   TRF_tpCondRet TRF_ConectarTarefas( tpTarefa ** ptTarefaSucessora, tpTarefa ** ptTarefaPredecessora )
+   {
+       LIS_tpCondRet retornoSucessor;
+       LIS_tpCondRet retornoPredecessor;
+       tpTarefa * pTarefaSucessora = (*ptTarefaSucessora);
+       tpTarefa * pTarefaPredecessora = (*ptTarefaPredecessora);
+
+       retornoSucessor = LIS_InserirElementoApos( pTarefaSucessora->tarefasPredecessoras , (void *) pTarefaPredecessora);
+       retornoPredecessor = LIS_InserirElementoApos( pTarefaPredecessora->tarefasSucessoras , (void *) pTarefaSucessora);
+        
+       return TRF_CondRetOK;
+
+   } /* Fim função: TRF Conectar tarefas */
+
+
+/***************************************************************************
+*
+*  Função: TRF Alterar Tarefa
+*  ****/
+
+   TRF_tpCondRet  TRF_AlterarTarefa( tpTarefa ** ptTarefa, char * novoNome, char * novaDescricao  )
+   {
+       tpTarefa * pTarefa = (*ptTarefa);
+
+       pTarefa->nome = (char*)malloc(strlen(novoNome));
+       pTarefa->descricao = (char*)malloc(strlen(novaDescricao));
+       strcpy(pTarefa->nome, novoNome);
+       strcpy(pTarefa->descricao, novaDescricao);
+
+       return TRF_CondRetOK ;
+
+   } /* Fim função: TRF Alterar tarefa */
+
+/***************************************************************************
+*
+*  Função: TRF Consultar Nome da Tarefa
+*  ****/
+
+   TRF_tpCondRet  TRF_ConsultarNomeTarefa( tpTarefa ** ptTarefa, char ** nomeConsultado ) 
+   {
+       tpTarefa * pTarefa = (*ptTarefa);
+
+       *nomeConsultado = (char*)malloc(strlen(pTarefa->nome));
+       strcpy(*nomeConsultado, pTarefa->nome);
+
+       return TRF_CondRetOK ;
+
+   } /* Fim função: TRF Consultar tarefa */
+
+/***************************************************************************
+*
+*  Função: TRF Consultar Descricao da Tarefa
+*  ****/
+
+   TRF_tpCondRet  TRF_ConsultarDescricaoTarefa( tpTarefa ** ptTarefa, char ** descricaoConsultada ) 
+   {
+       tpTarefa * pTarefa = (*ptTarefa);
+
+       *descricaoConsultada = (char*)malloc(strlen(pTarefa->descricao));
+       strcpy(*descricaoConsultada, pTarefa->descricao);
+
+       return TRF_CondRetOK ;
+
+   } /* Fim função: TRF Consultar tarefa */
+
+
+   static TRF_tpCondRet DesconectaTarefas( tpTarefa ** ptTarefa )
+   {
+       LIS_tpCondRet retPred;
+       LIS_tpCondRet retSuc;
+       tpTarefa * pTarefaCorrentePred;
+       tpTarefa * pTarefaCorrenteSuc;
+       int numPassos = 1;
+       tpTarefa * pTarefa = (*ptTarefa);
+
+       if(LIS_EstaVazia(pTarefa->tarefasPredecessoras) != LIS_CondRetListaVazia)
+       {
+           IrInicioLista(pTarefa->tarefasPredecessoras);
+           pTarefaCorrentePred = (tpTarefa *)LIS_ObterValor( pTarefa->tarefasPredecessoras );
+           IrInicioLista(pTarefaCorrentePred->tarefasSucessoras);
+
+           while(retPred != LIS_CondRetFimLista)
+           {
+               if(pTarefaCorrentePred->id == pTarefa->id)
+               {
+                   LIS_ExcluirElemento( pTarefaCorrentePred->tarefasSucessoras );
+                   break;
+               }
+           
+              retPred = LIS_AvancarElementoCorrente( pTarefaCorrentePred->tarefasSucessoras , numPassos );
+           }
+       }
+
+       if(LIS_EstaVazia(pTarefa->tarefasSucessoras) != LIS_CondRetListaVazia)
+       {
+           IrInicioLista(pTarefa->tarefasSucessoras);
+           pTarefaCorrenteSuc = (tpTarefa *)LIS_ObterValor( pTarefa->tarefasSucessoras );
+           IrInicioLista(pTarefaCorrenteSuc->tarefasPredecessoras);
+
+           while(retSuc != LIS_CondRetFimLista)
+           {
+               if(pTarefaCorrenteSuc->id == pTarefa->id)
+               {
+                   LIS_ExcluirElemento( pTarefaCorrenteSuc->tarefasPredecessoras );
+                   break;
+               }
+
+               retSuc = LIS_AvancarElementoCorrente( pTarefaCorrenteSuc->tarefasPredecessoras , numPassos );
+           }
+       }
+       return TRF_CondRetOK;
+   }
+
+/********** Fim do módulo de implementação: Módulo tarefa **********/
---
+++ T2/TESTTRF.C
@@ -0,0 +1,251 @@
+/***************************************************************************
+*  $MCI Módulo de implementação: Módulo de teste específico
+*
+*  Arquivo gerado:              TESTARV.C
+*  Letras identificadoras:      TARV
+*
+*  Nome da base de software:    Exemplo de teste automatizado
+*  Arquivo da base de software: D:\AUTOTEST\PROJETOS\SIMPLES.BSW
+*
+*  Projeto: T1 INF1301 2017.1
+*  Autores: mv Maurício Vieira
+*           sb Samuel Bastos
+*
+*  $ED Descrição do módulo
+*     Este mÇodulo contém as funções específicas para o teste do
+*     módulo árvore. Ilustra como redigir um interpretador de comandos
+*     de teste específicos utilizando o arcabouço de teste para C.
+*
+*  $EIU Interface com o usuário pessoa
+*     Comandos de teste específicos para testar o módulo árvore:
+*
+*     =criar        - chama a função ARV_CriarArvore( )
+*     =insdir <Char>
+*                   - chama a função ARV_InserirDireita( <Char> )
+*                     Obs. notação: <Char>  é o valor do parâmetro
+*                     que se encontra no comando de teste.
+*
+*     "=insesq" <Char>
+*                   - chama a função ARV_InserirEsquerda( <Char> )
+*     "=irpai"      - chama a função ARV_IrPai( )
+*     "=iresq"      - chama a função ARV_IrEsquerda( )
+*     "=irdir"      - chama a função ARV_IrDireita( )
+*     "=obter" <Char>
+*                   - chama a função ARV_ObterValorCorr( ) e compara
+*                     o valor retornado com o valor <Char>
+*     "=destroi"    - chama a função ARV_DestruirArvore( )
+*
+***************************************************************************/
+
+#include    <string.h>
+#include    <stdio.h>
+#include    <stdlib.h>
+
+#include    "TST_ESPC.H"
+
+#include    "generico.h"
+#include    "lerparm.h"
+
+#include    "tarefa.h"
+#include	"lista.h"
+
+/* Tabela dos nomes dos comandos de teste específicos */
+
+#define     CRIAR_TRF_CMD          "=criar"
+#define     DESTRUIR_TRF_CMD       "=destruir"
+#define     CONECTAR_TRF_CMD       "=conectar"
+#define     ALTERAR_TRF_CMD        "=alterar"
+#define     CONSULTAR_NOME_TRF_CMD "=consultarnome"
+#define     CONSULTAR_DESC_TRF_CMD "=consultardesc"
+
+#define STRING_DIM 100
+
+/* Vetor de árvores para serem usados nos testes */
+
+void * tarefas[10];
+
+/*****  Código das funções exportadas pelo módulo  *****/
+
+
+/***********************************************************************
+*
+*  $FC Função: TARV Efetuar operações de teste específicas para árvore
+*
+*  $ED Descrição da função
+*     Efetua os diversos comandos de teste específicos para o módulo
+*     árvore sendo testado.
+*
+*  $EP Parâmetros
+*     $P ComandoTeste - String contendo o comando
+*
+*  $FV Valor retornado
+*     Ver TST_tpCondRet definido em TST_ESPC.H
+*
+***********************************************************************/
+
+   TST_tpCondRet TST_EfetuarComando( char * ComandoTeste )
+   {
+
+      TRF_tpCondRet CondRetObtido   = TRF_CondRetOK ;
+      TRF_tpCondRet CondRetEsperada = TRF_CondRetFaltouMemoria ;
+                                      /* inicializa para qualquer coisa */
+
+      char ValorEsperado   = '?'  ;
+      char ValorObtido     = '!'  ;
+      char ValorDado       = '\0' ;
+	  int  TarefaObtida    = 11  ;
+      int  TarefaObtidaAux = 12;
+      char NomeObtido[STRING_DIM];
+      char DescricaoObtida[STRING_DIM];
+      char * NomeConsultado = (char*)malloc(sizeof(char)*100);
+      char * DescricaoConsultada = (char*)malloc(sizeof(char)*100);
+      int  NumLidos = -1 ;
+
+      TST_tpCondRet Ret ;
+
+      /* Testar ARV Criar árvore */
+
+         if ( strcmp( ComandoTeste , CRIAR_TRF_CMD ) == 0 )
+         {
+
+            NumLidos = LER_LerParametros( "issi" ,
+                               &TarefaObtida, NomeObtido, DescricaoObtida, &CondRetEsperada ) ;
+
+            if ( NumLidos != 4 )
+            {
+               return TST_CondRetParm ;
+            } /* if */
+
+			if ( TarefaObtida < 10 && TarefaObtida >= 0 )
+			{
+				CondRetObtido = TRF_CriarTarefa(&tarefas[TarefaObtida], NomeObtido, DescricaoObtida);
+			}
+			else
+			{
+				CondRetObtido = TRF_CondRetAcessoInvalidoVetor;
+			}
+
+            return TST_CompararInt( CondRetEsperada , CondRetObtido ,
+                                    "Retorno errado ao criar árvore." );
+
+         } /* fim ativa: Testar ARV Criar árvore */
+
+      /* Testar ARV Destruir árvore */
+
+         else if ( strcmp( ComandoTeste , DESTRUIR_TRF_CMD ) == 0 )
+         {
+			NumLidos = LER_LerParametros( "i", &TarefaObtida ) ;
+
+			if ( NumLidos != 1 )
+            {
+               return TST_CondRetParm ;
+            } /* if */
+
+            TRF_DestruirTarefa( &tarefas[TarefaObtida] ) ;
+
+            return TST_CondRetOK ;
+
+         } /* fim ativa: Testar ARV Destruir árvore */
+
+         else if ( strcmp( ComandoTeste, CONECTAR_TRF_CMD ) == 0 )
+         {
+            NumLidos = LER_LerParametros( "iii", &TarefaObtida, &TarefaObtidaAux, &CondRetEsperada ) ;
+
+            if( NumLidos != 3 )
+            {
+                return TST_CondRetParm ;
+            } /* if */
+
+			if ( TarefaObtida < 10 && TarefaObtida >= 0 && TarefaObtida != TarefaObtidaAux &&
+                 TarefaObtidaAux < 10 && TarefaObtidaAux >= 0)
+			{
+				CondRetObtido = TRF_ConectarTarefas(&tarefas[TarefaObtida], &tarefas[TarefaObtidaAux]);
+			}
+			else
+			{
+				CondRetObtido = TRF_CondRetAcessoInvalidoVetor;
+			}
+             
+            return TST_CompararInt( CondRetEsperada , CondRetObtido ,
+                                    "Retorno errado ao criar árvore." );
+         }
+
+         else if ( strcmp( ComandoTeste, ALTERAR_TRF_CMD ) == 0 )
+         {
+            NumLidos = LER_LerParametros( "issi" ,
+                               &TarefaObtida, NomeObtido, DescricaoObtida, &CondRetEsperada ) ;
+
+            if ( NumLidos != 4 )
+            {
+               return TST_CondRetParm ;
+            } /* if */
+
+			if ( TarefaObtida < 10 && TarefaObtida >= 0 )
+			{
+				CondRetObtido = TRF_AlterarTarefa(&tarefas[TarefaObtida], NomeObtido, DescricaoObtida);
+			}
+			else
+			{
+				CondRetObtido = TRF_CondRetAcessoInvalidoVetor;
+			}
+
+            return TST_CompararInt( CondRetEsperada , CondRetObtido ,
+                                    "Retorno errado ao criar árvore." );
+         }
+
+         else if ( strcmp( ComandoTeste, CONSULTAR_NOME_TRF_CMD ) == 0 )
+         {
+            NumLidos = LER_LerParametros( "ii" ,
+                               &TarefaObtida, &CondRetEsperada ) ;
+
+            if ( NumLidos != 2 )
+            {
+               return TST_CondRetParm ;
+            } /* if */
+
+			if ( TarefaObtida < 10 && TarefaObtida >= 0 )
+			{
+				CondRetObtido = TRF_ConsultarNomeTarefa(&tarefas[TarefaObtida], &NomeConsultado);
+			}
+			else
+			{
+				CondRetObtido = TRF_CondRetAcessoInvalidoVetor;
+			}
+
+            printf("%s \n", NomeConsultado);
+
+            return TST_CompararInt( CondRetEsperada , CondRetObtido ,
+                                    "Retorno errado ao criar árvore." );
+         }
+
+         else if ( strcmp( ComandoTeste, CONSULTAR_DESC_TRF_CMD ) == 0 )
+         {
+            NumLidos = LER_LerParametros( "ii" ,
+                               &TarefaObtida, &CondRetEsperada ) ;
+
+            if ( NumLidos != 2 )
+            {
+               return TST_CondRetParm ;
+            } /* if */
+
+			if ( TarefaObtida < 10 && TarefaObtida >= 0 )
+			{
+				CondRetObtido = TRF_ConsultarDescricaoTarefa(&tarefas[TarefaObtida], &DescricaoConsultada);
+			}
+			else
+			{
+				CondRetObtido = TRF_CondRetAcessoInvalidoVetor;
+			}
+
+            printf("%s \n", DescricaoConsultada);
+
+            return TST_CompararInt( CondRetEsperada , CondRetObtido ,
+                                    "Retorno errado ao criar árvore." );
+         }
+
+      return TST_CondRetNaoConhec ;	
+
+   } /* Fim função: TARV Efetuar operações de teste específicas para árvore */
+
+/********** Fim do módulo de implementação: Módulo de teste específico **********/
+
